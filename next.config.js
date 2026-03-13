fa42e041bd644f509f28a6aefa87a891811d858b
TheKazuto <watarizangado@gmail.com>
2026-03-13 15:08:41 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'coin-images.coingecko.com' },
       { protocol: 'https', hostname: 'raw.githubusercontent.com' },
       { protocol: 'https', hostname: 'api.geckoterminal.com' },
+      { protocol: 'https', hostname: 'assets.geckoterminal.com' },  // token images
       { protocol: 'https', hostname: 'ipfs.io' },
       { protocol: 'https', hostname: 'gateway.pinata.cloud' },
       { protocol: 'https', hostname: 'icons.llamao.fi' },
